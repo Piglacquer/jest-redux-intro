465740f5d9a0cff980d206e5653aa568764e48b4
Patrick Biffle <pbiffle@gmail.com>
2019-03-06 11:04:20 -0700

    added default props to test setup

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Congrats = (props) => {
   return (
@@ -8,6 +9,10 @@ const Congrats = (props) => {
       </div>
       : <div data-test='component-congrats' />
   )
+}
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
 }
 
 export default Congrats
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+import checkPropTypes from 'check-prop-types'
 
-import { findByTestAttr } from '../test/testUtils'
+import { findByTestAttr, checkProps } from '../test/testUtils'
 import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />)
+const defaultProps = { success: false }
+
+const setup = (props = { success: false }) => {
+  const setupProps = { ...defaultProps, ...props }
+  return shallow(<Congrats {...setupProps} />)
 }
 
 test('renders without error', () => {
-  const wrapper = setup()
+  const wrapper = setup({ success: false })
   const component = findByTestAttr(wrapper, 'component-congrats')
   expect(component.length).toBe(1)
 })
-test('renders no text when "success" prop is false', () => {
+test('renders no text when `success` prop is false', () => {
   const wrapper = setup({ success: false })
   const component = findByTestAttr(wrapper, 'component-congrats')
   expect(component.text()).toBe('')
@@ -25,4 +29,9 @@ test('renders non-empty congrats message when `success` prop is true', () => {
   const wrapper = setup({ success: true })
   const message = findByTestAttr(wrapper, 'congrats-message')
   expect(message.text().length).not.toBe(0)
+})
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: false }
+  const propError = checkProps(Congrats, expectedProps)
+  expect(propError).toBeUndefined()
 })
