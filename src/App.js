c2b52e4e7466f666188330d52b655fd82ae76edd
Patrick Biffle <pbiffle@gmail.com>
2019-03-07 13:54:41 -0700

    completed initial redux setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import GuessedWords from './GuessedWords'
+import Congrats from './Congrats'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1 className='heading'>Game, Yo</h1>
+        <Congrats success={false} />
+        <GuessedWords guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]} />
       </div>
     )
   }
--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,22 +1,28 @@
 import React from 'react'
+import './styles/GuessedWords.css'
 import PropTypes from 'prop-types'
 
 const renderGuessedWords = (arrayOfWords) => {
   return arrayOfWords.map((word, i) => {
     return (
-      <div data-test='guessed-word' key={word.guessedWord + i}>
+      <div className='guessed-word' data-test='guessed-word' key={word.guessedWord + i}>
         <h3>{word.guessedWord}</h3>
         <span>{word.letterMatchCount}</span>
       </div>
     )
   })
 }
+
 const GuessedWords = (props) => {
   return (
     <div data-test='component-guessed-words'>
       { props.guessedWords.length === 0
         ? <span data-test='guess-instructions'>Attempt to guess the secret word!</span>
         : <div data-test='guessed-words'>
+          <div className='scoreboard-header'>
+            <h2>Guessed Words</h2>
+            <h2>Letter Match Count</h2>
+          </div>
           {renderGuessedWords(props.guessedWords)}
         </div>
       }
