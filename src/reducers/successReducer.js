de5bbb3a280f63b3e21f33845bf1e701d77372fd
Patrick Biffle <pbiffle@gmail.com>
2019-03-11 14:20:00 -0600

    added switch statement and basic tests to successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,10 @@
-export default (state, action) => {
-  return null
+import { actionTypes } from '../actions'
+
+export default (state = false, action) => {
+  switch (action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true
+    default:
+      return state
+  }
 }
